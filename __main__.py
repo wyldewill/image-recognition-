8b6e5ca33cb76aec41ce8199dff22ff1616f8fdb
William <wyldewill1@gmail.com>
2019-07-12 10:14:40 +0000

    preparing for demo

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-#Import Modules
+# Import Modules
 from edgetpu.detection.engine import DetectionEngine
 from imutils.video import VideoStream
 from PIL import Image
@@ -11,7 +11,8 @@ import numpy as np
 
 # Parse Args
 ap = argparse.ArgumentParser()
-ap.add_argument("-m", "--model", default="mobilenet_ssd_v2/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite", help="path to TensorFlow Lite object detection model")
+ap.add_argument("-m", "--model", default="mobilenet_ssd_v2/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite",
+                help="path to TensorFlow Lite object detection model")
 ap.add_argument("-l", "--labels", default="mobilenet_ssd_v2/coco_labels.txt", help="path to labels file")
 ap.add_argument("-c", "--confidence", type=float, default=0.35, help="minimum probability to filter weak detections")
 ap.add_argument("-w", "--width", type=int, default=700, help="width of frame")
@@ -19,11 +20,11 @@ args = vars(ap.parse_args())
 
 
 # Dialog Box Func
-def dialogBox(title, text,  details="", width=400, height=100):
+def dialogBox(title, text, details="", width=400, height=100):
     img = np.zeros((height, width, 3), np.uint8)
-    img[:,0:width] = (100, 100, 200)
-    cv2.putText(img, text, (0, height//2), cv2.FONT_HERSHEY_DUPLEX, 1, (10,10,10), 2)
-    cv2.putText(img, details, (0, (30+height//2)), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 2)
+    img[:, 0:width] = (100, 100, 200)
+    cv2.putText(img, text, (0, height // 2), cv2.FONT_HERSHEY_DUPLEX, 1, (10, 10, 10), 2)
+    cv2.putText(img, details, (0, (30 + height // 2)), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 2)
     cv2.imshow(title, img)
     cv2.waitKey(0)
 
@@ -56,7 +57,7 @@ else:
 
 # Control Panel
 def updateThreshold(x):
-    args["confidence"] = x/100
+    args["confidence"] = x / 100
 
 
 def updateSize(x):
@@ -64,15 +65,14 @@ def updateSize(x):
 
 
 cv2.namedWindow("Controls", cv2.WINDOW_NORMAL)
-cv2.createTrackbar("Threshold", 'Controls', int(args['confidence']*100), 100,  updateThreshold)
-cv2.createTrackbar("Size", 'Controls', int(args['width']),  1000,  updateSize)
+cv2.createTrackbar("Threshold", 'Controls', int(args['confidence'] * 100), 100, updateThreshold)
+cv2.createTrackbar("Size", 'Controls', int(args['width']), 2000, updateSize)
 
 
 # Draw Func
 def drawFrame():
-
     thresh = args['confidence']
-    
+
     frame = vs.read()
     frame = imutils.resize(frame, width=args["width"])
     frame = cv2.flip(frame, 1)
@@ -89,13 +89,14 @@ def drawFrame():
         (startX, startY, endX, endY) = box
         label = labels[r.label_id]
 
-        colour = (0, 255*r.score, 255*(1-r.score))
+        colour = (0, 255 * r.score, 255 * (1 - r.score))
         cv2.rectangle(orig, (startX, startY), (endX, endY), colour, 2)
         y = startY - 15 if startY - 15 > 15 else startY + 15
         text = f"{label}: {int(r.score * 100)}%"
         cv2.putText(orig, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, colour, 2)
 
-    cv2.putText(orig, f"{int(thresh*100)}% Threshold", (args["width"]-130, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255*thresh, 255*(1-thresh)), 2)
+    cv2.putText(orig, f"{int(thresh * 100)}% Threshold", (args["width"] - 130, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                (0, 255 * thresh, 255 * (1 - thresh)), 2)
 
     cv2.imshow("Image Recognition", orig)
 
